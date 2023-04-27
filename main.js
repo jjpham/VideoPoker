b22f39892186a7b183e58024c67173f1907eefa7
jjpham <phamjason96@gmail.com>
2023-04-26 23:54:49 -0700

    added toggleCard functions and render cards also structured buttons

--- main.js
+++ main.js
@@ -14,66 +14,85 @@ function buildOriginalDeck() {
     return deck;
 }
 
-
-const tempDeck = [...originalDeck];
+let tempDeck = [...originalDeck];
 const pulledCard = [];
+const pulledCardTwo = [];
 
 let hand;
 let newCard;
 let secondDeal;
 let winningHand;
+let deal;
 
-//document.getElementById('cardContainer').addEventListener("click", handleDrop);
 hand = document.getElementsByClassName('card');
+document.getElementById('deal').addEventListener('click', dealCards);
 
 init();
 function init(){
-//hand = new Array(5).fill(null);
 secondDeal = -1;
 winningHand = null;
-render();
 }
-
 
 function addCardEvt(){
     for (i = 0; i < hand.length; i++){
         if(secondDeal === -1){
-            hand[i].addEventListener("click",addChgCard);}
-        else if(secondDeal === 1){
-            hand[i].removeEventListener("click", addChgCard);}
+            hand[i].addEventListener("click",addChgCard);
+            console.log('Add Eventlistener FromCard');
         }
-
+        else if(secondDeal === 1){
+            hand[i].removeEventListener("click", addChgCard);
+            console.log('Remove Eventlistener FromCard');
+            }
+        }
 }
 function addChgCard(){
-    if(this.chgCard){
-        this.chgCard = null;
-    }
-    else{
-    this.chgCard = 1;
-    }
-    
-    // console.log(hand);
-    //  console.log(this);
+    if(this.chgCard){this.chgCard = null;}
+    else{this.chgCard = 1;}
     console.log(this.chgCard);
     console.log(this.card);
 }
 
-
-function render(){
+function dealCards(){
+    console.log(secondDeal);
     if(secondDeal === -1){
-        setFirstHand();
+        resetHandRender();
         addCardEvt();
+        setHand();
+        renderHand();
     }
     else if(secondDeal === 1){
         addCardEvt();
-        
+        setHand();
+        renderHand();
+        tempDeck = [...originalDeck];
+    }
+    secondDeal *= -1;
+}
+function setHand(){
+
+    for(let i = 0; i <hand.length; i++){
+        if(secondDeal === -1){
+        hand[i].card = getNewCard();
+        }
+        else if(secondDeal ===1){
+            if(hand[i].chgCard === null){
+                hand[i].card = getNewCard();
+                hand[i].className = 'card';
+                hand[i].classList.add(hand[i]['card'][0]['face']);
+            }
+        }
+    } 
+}
+function resetHandRender(){
+    for(let i = 0; i < hand.length; i++){
+        hand[i].className = 'card';
+        hand[i].chgCard = null;
     }
 }
-function setFirstHand(){
-    for(i = 0; i <hand.length; i++){
-        hand[i].card = getNewCard();
+function renderHand(){
+    for(let i = 0; i < hand.length; i++){
+        hand[i].classList.add(hand[i]['card'][0]['face']);
     }
-    
 }
 //This line should be in a function
 //const playerHand = document.getElementById('chgCard');
